90947e68b2186938e70f33219b1c4bc4328045df
hungnv6933 <hungnv6933@co-well.com.vn>
2019-01-09 14:34:40 +0700

    update code

--- public/libs/ckeditor/config.js
+++ public/libs/ckeditor/config.js
@@ -46,4 +46,14 @@ CKEDITOR.editorConfig = function( config ) {
 
     // Simplify the dialog windows.
     config.removeDialogTabs = 'image:advanced;link:advanced';
+
+    var domain;
+    domain = window.location.protocol + '//' + window.location.hostname + '/admin/assets/global/plugins/';
+
+    config.filebrowserBrowseUrl = domain + 'ckfinder/ckfinder.html';
+    config.filebrowserImageBrowseUrl = domain + 'ckfinder/ckfinder.html?type=Images';
+    config.filebrowserFlashBrowseUrl = domain + 'ckfinder/ckfinder.html?type=Flash';
+    config.filebrowserUploadUrl = domain + 'ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
+    config.filebrowserImageUploadUrl =  domain +'ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
+    config.filebrowserFlashUploadUrl = domain + 'ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
 };
