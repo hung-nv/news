54a55474a58fc6a091f7e2c1671eecd91742bffd
hungnv6933 <hungnv6933@co-well.com.vn>
2019-01-11 17:34:11 +0700

    update code

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,52 @@
 <div id="footer">
-    <div class="infor">
-		@if(!empty($setting['company_name']))
-            {{ $setting['company_name'] }}
-        @endif
+    <div class="container">
+        <div class="row">
+            <div class="menu-bottom">
+                <div class="company">
+                    <h6>Về chúng tôi</h6>
+                    <p>Những điều quen thuộc với người này có thể là một BÍ MẬT to lớn đối với người khác</p>
+                    <p>Liên hệ chúng tôi: contact@gocteen.com</p>
+                    <p>GP: 354/GP-TTĐT do Sở TT&TT Hà Nội cấp 25/01/2017</p>
+                </div>
+
+                <ul>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                </ul>
+
+                <ul>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                </ul>
+
+                <ul>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                </ul>
+
+                <ul>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                    <li><h6><a href="#">Tin tức</a></h6></li>
+                </ul>
+                <div class="clear"></div>
+            </div>
+            <div class="copyright">
+                <span class="sp-copyright">
+                    @Copyright by GocTeen
+                </span>
+            </div>
+        </div>
     </div>
 </div>
