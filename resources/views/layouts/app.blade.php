5bcc210d94b818d3348331abe4465eb7b70f7fb4
hungnv6933 <hungnv6933@co-well.com.vn>
2019-01-14 11:33:05 +0700

    update code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,14 +16,6 @@
     <meta property="og:title" content="@yield('og_title')" />
     <meta property="og:description" content="@yield('og_description')" />
     <meta property="og:image" content="@yield('og_image')" />
-    
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-9491879653600025",
-            enable_page_level_ads: true
-        });
-    </script>
 </head>
 <body>
     @include('layouts.header')
--- resources/views/mobile/layouts/appMobile.blade.php
+++ resources/views/mobile/layouts/appMobile.blade.php
@@ -11,14 +11,6 @@
     <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/app-mobile.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/responsivemobilemenu.css') }}">
-
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-9491879653600025",
-            enable_page_level_ads: true
-        });
-    </script>
 </head>
 <body id="@yield('pageId')">
 @include('mobile.layouts.header')
